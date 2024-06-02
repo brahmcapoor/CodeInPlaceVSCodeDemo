7ef8b10db030eee59862d8de643c91c6f60b52e7
Brahm Capoor <brahmcapoor@gmail.com>
2024-06-03 00:35:00 +0100

    first commit

---
+++ my_second_program.py
@@ -0,0 +1,8 @@
+
+def convert_c_to_f(temp_in_celsius):
+    temp_in_farenheight = temp_in_celsius * 1.8 + 32
+    return temp_in_farenheight
+    
+
+temperature_in_f = convert_c_to_f(-40)
+print(temperature_in_f)
